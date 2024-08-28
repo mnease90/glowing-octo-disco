ced57710e2cb356af64a781831ce7b1401138377
mnease90 <mnease90@users.noreply.github.com>
2024-08-27 22:39:29 -0700

    Solve challenge 2

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -4,6 +4,56 @@
 
 module.exports = (logSources, printer) => {
   return new Promise((resolve, reject) => {
-    resolve(console.log("Async sort complete."));
+    drainAsyncLogs(logSources, printer, resolve)
   });
 };
+
+const drainAsyncLogs = async (logSources, printer, resolve) => {
+  let initialEntries = await Promise.all(logSources.map(async (ls, index) => {
+    let entry = await ls.popAsync();
+    return {
+      index: index,
+      entry: entry
+    };
+  }));
+
+  let sortedEntries = initialEntries.sort((a, b) => a.entry.date - b.entry.date);
+
+  let earliestEntry;
+
+  // Still sources to extract from
+  while(sortedEntries.length > 0) {
+    earliestEntry = sortedEntries.shift()
+    printer.print(earliestEntry.entry)
+
+    let earliestEntryIx = earliestEntry.index;
+    let nextEntry = await logSources[earliestEntryIx].popAsync();
+
+    if (nextEntry === false) {
+      continue;
+    }
+
+    
+    let entryToInsert = { index: earliestEntryIx, entry: nextEntry };
+    insertIntoSortedEntries(entryToInsert, sortedEntries)
+  }
+
+  printer.done();
+  resolve(console.log("Async sort complete."))
+}
+
+let insertIntoSortedEntries = (entryToInsert, sortedEntries) => {
+  let ix = 0;
+
+  while (ix < sortedEntries.length) {
+    if ( entryToInsert.entry.date <= sortedEntries[ix].entry.date) {
+      sortedEntries.splice(ix, 0, entryToInsert)
+      break;
+    } else {
+      ix++;
+    }
+  }
+  if (ix === sortedEntries.length) {
+    sortedEntries.push(entryToInsert)
+  }
+}
