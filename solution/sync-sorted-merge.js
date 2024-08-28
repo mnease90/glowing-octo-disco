d1951f7d923a46fc0d1cd126d6a078d88ca7321e
mnease90 <mnease90@users.noreply.github.com>
2024-08-27 21:55:07 -0700

    Solve challenge 1

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -3,5 +3,50 @@
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
+  let initialEntries = logSources.map((ls, index) => {
+    let entry = ls.pop();
+    return {
+      index: index,
+      entry: entry
+    };
+  });
+
+  let sortedEntries = initialEntries.sort((a, b) => a.entry.date - b.entry.date);
+  let earliestEntry;
+
+  // Still sources to extract from
+  while(sortedEntries.length > 0) {
+    earliestEntry = sortedEntries.shift()
+    printer.print(earliestEntry.entry)
+
+    let earliestEntryIx = earliestEntry.index;
+    let nextEntry = logSources[earliestEntryIx].pop();
+
+    if (nextEntry === false) {
+      continue;
+    }
+
+    
+    let entryToInsert = { index: earliestEntryIx, entry: nextEntry };
+    insertIntoSortedEntries(entryToInsert, sortedEntries)
+  }
+
+  printer.done();
   return console.log("Sync sort complete.");
 };
+
+let insertIntoSortedEntries = (entryToInsert, sortedEntries) => {
+  let ix = 0;
+
+  while (ix < sortedEntries.length) {
+    if ( entryToInsert.entry.date <= sortedEntries[ix].entry.date) {
+      sortedEntries.splice(ix, 0, entryToInsert)
+      break;
+    } else {
+      ix++;
+    }
+  }
+  if (ix === sortedEntries.length) {
+    sortedEntries.push(entryToInsert)
+  }
+}
